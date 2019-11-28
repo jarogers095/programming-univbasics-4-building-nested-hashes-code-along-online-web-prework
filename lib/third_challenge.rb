9fff73b89901869a833eaab906f43412a5237673
James Rogers <jarogers095@gmail.com>
2019-11-28 23:33:39 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,7 @@ def third_challenge
        hero: {
          name: "Romeo",
          age: "15",
-         status: "Alive"
+         status: "alive"
        },
        hero_friends: []
      },
@@ -25,7 +25,7 @@ def third_challenge
        heroine: {
          name: "Juliet",
          age: "15",
-         status: "Alive"
+         status: "alive"
        },
        heroine_friends: []
      }
